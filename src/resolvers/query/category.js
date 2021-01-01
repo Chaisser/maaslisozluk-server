c065293ff509e3c51041cb1adf45a846993a4c60
Doruk Karaboncuk <dorukkaraboncuk@Doruk-iMac.local>
2021-01-02 00:28:19 +0300

    category

--- src/resolvers/query/category.js
+++ src/resolvers/query/category.js
@@ -1,6 +1,10 @@
 const categoryQuery = {
   categories(parent, args, { prisma }, info) {
-    return prisma.query.categories({}, info);
+    const opArgs = {};
+    if (args.first) {
+      opArgs.first;
+    }
+    return prisma.query.categories(opArgs, info);
   },
   category(parent, args, { prisma }, info) {
     return prisma.query.category(
