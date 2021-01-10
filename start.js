29a90049cdb74872e2ef67976eebf3ae5124106a
Doruk Karaboncuk <dorukkaraboncuk@Doruk-iMac.local>
2021-01-11 01:38:44 +0300

    deploy deneme

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
-require('babel-register')
+require("babel-register");
 require("babel-core/register");
 require("babel-polyfill"); // If you use async and await
 // This us where our main application index path is
-require('./src/index');
+require("./src/index");
+// Bu yeni bir comment
