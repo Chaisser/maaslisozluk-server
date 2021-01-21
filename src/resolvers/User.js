f1b866c4047c4c8c2430ba2ab64622e7a0692143
Doruk Karaboncuk <dorukkaraboncuk@Doruk-iMac.local>
2021-01-22 02:08:02 +0300

    privacy settings

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,4 +1,33 @@
+import getUserId from "./../utils/getUserId";
+
 const User = {
+  username: {
+    async resolve(parent, args, { request, prisma }, info) {
+      const user = getUserId(request, false);
+      if (user.userId && parent.id === user.userId) {
+        return parent.username;
+      }
+      return "not-permitted";
+    },
+  },
+  email: {
+    async resolve(parent, args, { request, prisma }, info) {
+      const user = getUserId(request, false);
+      if (user.userId && parent.id === user.userId) {
+        return parent.email;
+      }
+      return "not-permitted";
+    },
+  },
+  phoneNumber: {
+    async resolve(parent, args, { request, prisma }, info) {
+      const user = getUserId(request, false);
+      if (user.userId && parent.id === user.userId) {
+        return parent.phoneNumber;
+      }
+      return "not-permitted";
+    },
+  },
   topicsCount: {
     async resolve(parent, args, { request, prisma }, info) {
       const result = await prisma.query.topicsConnection(
